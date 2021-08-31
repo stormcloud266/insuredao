ec4228dc5a9d21fc4a2e57ef81ae5937e7beb78e
Tawnee <allprt89@gmail.com>
2021-08-31 11:53:57 -0600

    add footer styles

--- src/components/global/footer/footer.js
+++ src/components/global/footer/footer.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import NavLinks from '@global/navLinks'
+import { Container, Flex } from '@UI'
+import { Twitter, Medium, Telegram, Discord } from '@images/icons'
 import * as styles from './footer.module.scss'
 
 const Footer = () => (
 	<footer className={styles.footer}>
-		<div className='wrapper'>
-			<nav className={styles.nav}>
-				<NavLinks />
-			</nav>
-		</div>
+		<Container wrapper>
+			<Flex className={styles.inner}>
+				<p className={styles.text}>© 2021 InsureDAO. All Rights Reserved.</p>
+				<div className={styles.socialContainer}>
+					<a href='https://example.com' target='_blank' rel='noreferrer'>
+						<Twitter />
+					</a>
+					<a href='https://example.com' target='_blank' rel='noreferrer'>
+						<Medium />
+					</a>
+					<a href='https://example.com' target='_blank' rel='noreferrer'>
+						<Telegram />
+					</a>
+					<a href='https://example.com' target='_blank' rel='noreferrer'>
+						<Discord />
+					</a>
+				</div>
+			</Flex>
+		</Container>
 	</footer>
 )
 
