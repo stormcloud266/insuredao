98e760bde0e7bf7e637dd230caafd5b1a5484331
Tawnee <allprt89@gmail.com>
2021-09-03 10:01:35 -0600

    adjust button hover. replace bgimage with static image

--- src/components/home/hero/hero.js
+++ src/components/home/hero/hero.js
@@ -1,55 +1,40 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import { getImage, GatsbyImage } from 'gatsby-plugin-image'
-import { convertToBgImage } from 'gbimage-bridge'
-import BackgroundImage from 'gatsby-background-image'
 import { Container, Title, Button } from '@UI'
 import * as styles from './hero.module.scss'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Hero = () => {
-	const { placeholderImage } = useStaticQuery(
-		graphql`
-			query {
-				placeholderImage: file(relativePath: { eq: "InsureDAO_hero.png" }) {
-					childImageSharp {
-						gatsbyImageData(
-							layout: FULL_WIDTH
-							placeholder: BLURRED
-							quality: 100
-						)
-					}
-				}
-			}
-		`
-	)
-	const image = getImage(placeholderImage)
-	const bgImage = convertToBgImage(image)
 	return (
-		<BackgroundImage
-			Tag='section'
-			className={styles.background}
-			// Spread bgImage into BackgroundImage:
-			{...bgImage}
-			preserveStackingContext
-		>
-			<div className={styles.container}>
-				<Container wrapper>
-					<Title black center margin tag='h1'>
-						The Open Insurance Protocol
-					</Title>
-					<p className={styles.subtitle}>
-						Allowing anyone to create an insurance market for their DeFi
-						protocol & ensure it’s security...
-					</p>
-					<div className={styles.buttonContainer}>
-						<Button href='https://example.com'>Launch App</Button>
-						<Button href='https://insuredao.gitbook.io/insuredao/' white>
-							Explore Docs
-						</Button>
-					</div>
-				</Container>
+		<div className={styles.container}>
+			<div className={styles.background}>
+				<StaticImage
+					src='../../../assets/images/InsureDAO_hero.png'
+					alt=''
+					layout='fullWidth'
+					quality={100}
+					objectFit='cover'
+					objectPosition='center bottom'
+					style={{ position: 'static' }}
+					placeholder='blurred'
+				/>
 			</div>
-		</BackgroundImage>
+
+			<Container wrapper>
+				<Title black center margin tag='h1'>
+					The Open Insurance Protocol
+				</Title>
+				<p className={styles.subtitle}>
+					Allowing anyone to create an insurance market for their DeFi protocol
+					& ensure it’s security...
+				</p>
+				<div className={styles.buttonContainer}>
+					<Button href='https://example.com'>Launch App</Button>
+					<Button href='https://insuredao.gitbook.io/insuredao/' white>
+						Explore Docs
+					</Button>
+				</div>
+			</Container>
+		</div>
 	)
 }
 
